6d094bc0d50468b36ab4ef0a1cb6b3fdcda7e2c3
Ben Smith <binjimin@gmail.com>
2024-03-04 15:44:09 -0800

    Fix crash when audio can't be created

--- src/binjnes.c
+++ src/binjnes.c
@@ -711,15 +711,17 @@ static void frame(void)  {
     // Clear audio buffer.
     size_t read_head = atomic_load_size(&s_audio_buffer_read);
     size_t write_head = atomic_load_size(&s_audio_buffer_write);
-    if (write_head < read_head) {
-      memset(s_audio_buffer + write_head, 0,
-             (read_head - write_head - 1) * sizeof(f32));
-    } else {
-      size_t to_end = ARRAY_SIZE(s_audio_buffer) - write_head;
-      memset(s_audio_buffer + write_head, 0, to_end * sizeof(f32));
-      memset(s_audio_buffer, 0, (read_head - 1) * sizeof(f32));
+    if (read_head > 0) {
+      if (write_head < read_head) {
+        memset(s_audio_buffer + write_head, 0,
+               (read_head - write_head - 1) * sizeof(f32));
+      } else {
+        size_t to_end = ARRAY_SIZE(s_audio_buffer) - write_head;
+        memset(s_audio_buffer + write_head, 0, to_end * sizeof(f32));
+        memset(s_audio_buffer, 0, (read_head - 1) * sizeof(f32));
+      }
+      atomic_store_size(&s_audio_buffer_write, read_head - 1);
     }
-    atomic_store_size(&s_audio_buffer_write, read_head - 1);
   } else {
     f64 delta_sec = sapp_frame_duration();
     Ticks delta_ticks = (Ticks)(delta_sec * PPU_TICKS_PER_SECOND);
