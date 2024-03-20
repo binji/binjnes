binji/binjnes

commit 7b26ac8bf06ea4324b089b065ce0ee3b147720d7

Update noise channel at CPU rate (like triangle)
It seems to sound better, thanks Zeta!

src/emulator.c
@@ -842,7 +842,7 @@ static void set_period_hi(A* a, int chan, u8 val) {
 }
 
 static void apu_tick(E *e, u64 cy) {
-  #define TIMER_DIFF V128_MAKE_U16(1, 1, 2, 1, 1, 0, 0, 0)
+  #define TIMER_DIFF V128_MAKE_U16(1, 1, 2, 2, 1, 0, 0, 0)
   static const u8 pduty[][8] = {{0, 1, 0, 0, 0, 0, 0, 0},
                                 {0, 1, 1, 0, 0, 0, 0, 0},
                                 {0, 0, 0, 0, 1, 1, 1, 1},

commit 48a1d433988b9c12c43cef4249dab6c8b2c9cb50

Fix noise channel frequency
I broke it with 7b26ac8bf06ea4324b089b065ce0ee3b147720d7, it sounded
better to me, but it was wrong :)

src/emulator.c
@@ -1026,7 +1026,7 @@ static void apu_tick(E *e, u64 cy, bool odd) {
   A* a = &e->s.a;
   AudioBuffer* ab = &e->audio_buffer;
   u16x8 timer_diff = odd ? V128_MAKE_U16(1, 1, 1, 1, 1, 0, 0, 0)
-                         : V128_MAKE_U16(0, 0, 1, 1, 0, 0, 0, 0);
+                         : V128_MAKE_U16(0, 0, 1, 0, 0, 0, 0, 0);
   u16x8 timer0 = v128_lt_u16(a->timer, timer_diff);
   a->timer = v128_blendv(v128_sub_u16(a->timer, timer_diff), a->period, timer0);
   timer0 = v128_and(timer0, a->play_mask);